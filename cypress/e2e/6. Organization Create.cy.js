0980c1924a2312c253d9cc8fbda8455dafc404b0
gosl8908 <gosl8908@gmail.com>
2023-11-03 12:24:21 +0900

    password 변경

--- cypress/e2e/6. Organization Create.cy.js
+++ cypress/e2e/6. Organization Create.cy.js
@@ -91,7 +91,7 @@ describe('Organization Create', () => {
     cy.viewport(1920, 1080); // FHD 해상도 설정
     cy.visit(Cypress.env('prodadmin')) 
     cy.get('#username').type(Cypress.env('id')); // 이메일 입력
-    cy.get('#password').type(Cypress.env('Password')); // 비밀번호 입력
+    cy.get('#password').type(Cypress.env('password')); // 비밀번호 입력
     cy.get('#kc-login').click() // 로그인 선택
     cy.wait(3000);
 
@@ -124,7 +124,7 @@ describe('Organization Create', () => {
     cy.visit(Cypress.env('prod')) 
     cy.contains('로그인').click(); // 로그인 클릭
     cy.get('#username').type(Cypress.env('auto_test_id')); // 이메일 입력
-    cy.get('#password').type(Cypress.env('Password')); // 비밀번호 입력
+    cy.get('#password').type(Cypress.env('password')); // 비밀번호 입력
     cy.get('#kc-login').click() // 로그인 선택
     cy.wait(3000);
 
