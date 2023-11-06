f01f1724f28423652a866c8de605e53fdbfa0585
gosl8908 <gosl8908@gmail.com>
2023-11-06 15:26:14 +0900

    api 호출 코드 변경

--- cypress/e2e/workspace/api test.cy.js
+++ cypress/e2e/workspace/api test.cy.js
@@ -1,44 +1,21 @@
 describe('인퍼런스 서비스 api test', () => {
-
-  before(() => {
-    // cy.setDateToEnv();
-    // cy.getAllCookies(); // 쿠키 삭제
-    // cy.getAllLocalStorage(); // 로컬 삭제
-    // cy.getAllSessionStorage(); // 세션 삭제
-    // cy.viewport(1920, 1080); // FHD 해상도 설정
-  });
-
-  
-
   it('인퍼런스 서비스 api test', () => {
-    let decodedFileContent; // decodedFileContent 변수를 선언
+    cy.fixture("자동화용 데이터셋.csv").then((fileContent) => {
+      const formData = new FormData(); // FormData 생성
 
-    cy.fixture("dfgfdgdf_sample_20231103_163450.csv").then((fileContent) => {
+      // 파일을 FormData에 추가
+      formData.append('file', new Blob([fileContent], { type: 'text/csv' }), '자동화용 데이터셋.csv');
+
       cy.request({
         method: "POST",
-        url: "https://dev-inference.deepphi.ai/dfgfdgdf/1.3/api/inference",
-        failOnStatusCode: false, // 실패 상태 코드 무시
+        url: "https://dev-inference.deepphi.ai/12d2d112dd21/1.1/api/inference",
+        failOnStatusCode: false,
         headers: {
-          "Content-Type": "multipart/form-data",
-          "Accept-Encoding": "gzip, deflate, br",
-          "User-Agent": "Chrome/91.0.4472.124", // 원하는 User-Agent 문자열 사용
-          "Connection": "keep-alive", // Connection 헤더 추가
+          'Content-Type': 'multipart/form-data', // Content-Type을 multipart/form-data로 설정
         },
-        form: true, // form 데이터를 사용할 것임을 명시
-        
-        body: {
-          file: {
-            value: decodedFileContent, // 디코딩된 파일 내용 사용
-            options: {
-              filename: 'dfgfdgdf_sample_20231103_163450.csv',
-              mimeType: 'text/csv',
-          },
-        },
-      },
+        body: formData, // FormData를 요청 본문으로 사용
       }).then((response) => {
-        // 여기에서 API 응답을 검사하고 원하는 테스트를 수행
-        
-        expect(response.status).to.eq(200); // 예상되는 HTTP 상태 코드
+        expect(response.status).to.eq(200);
       });
     });
   });
--- cypress/e2e/workspace/test.cy.js
+++ cypress/e2e/workspace/test.cy.js
@@ -53,40 +53,6 @@ describe('SignUp Test', () => {
 //     cy.wait(20000); // 20초 대기
 //     cy.contains('샘플데이터'); // 업로드 정상 체크
 
-    // // api 호출
-    // cy.fixture("glass1.jpg").as('imageFile').then((fileContent) => {
-    //   cy.request({
-    //     method: "POST",
-    //     url: "https://inference.deepphi.ai/aas/1.0/api/prediction",
-    //     headers: {
-    //       "Content-Type": "multipart/form-data",
-    //       "Accept-Encoding": "gzip, deflate, br",
-    //       "User-Agent": "Chrome/91.0.4472.124", // 원하는 User-Agent 문자열 사용
-    //       "Connection": "keep-alive", // Connection 헤더 추가
-    //     },
-    //     form: true, // form 데이터를 사용할 것임을 명시
-    //     body: {
-    //       requestFile: {
-    //         value: {
-    //           fileContent,
-    //             filePath: 'C:\\my-cypress-project\\cypress\\fixtures\\glass1.jpg',
-    //             fileName: 'glass1.jpg',
-    //             mimeType: 'image/jpeg'
-    //           // filename: "glass1.jpg",
-    //           // mimeType: "image/jpeg",
-    //         },
-    //         formData: true, // 파일 업로드를 위해 formData를 사용
-    //         failOnStatusCode: false, // 에러가 발생해도 계속 진행
-    //         timeout: 10000, // 요청 타임아웃 설정
-    //         retryOnNetworkFailure: true, // 네트워크 실패 시 재시도
-    //       },
-    //     },
-    //   }).then((response) => {
-    //     // 여기에서 API 응답을 검사하고 원하는 테스트를 수행
-    //     expect(response.status).to.eq(200); // HTTP 상태 코드
-    //   });
-    // });
-
     emailModule.email(Cypress.env('emailtitle'), Cypress.env('emailbody'));
   });
 });
