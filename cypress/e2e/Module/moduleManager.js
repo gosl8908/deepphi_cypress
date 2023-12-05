c6f65bb9bc5d8cae0dd2d27f6dc13c71822a9fc6
gosl8908 <gosl8908@gmail.com>
2023-12-05 11:07:21 +0900

    main 변경

--- cypress/e2e/Module/moduleManager.js
+++ cypress/e2e/Module/moduleManager.js
@@ -1,13 +1,13 @@
 const loginModule = require('./login.js');
 const adminloginModule = require('./adminlogin.js');
 const emailModule = require('./email.js');
-const Record_apiModule = require('./record_api.js');
+const record_apiModule = require('./record_api.js');
 // 다른 모듈들도 필요한 경우 추가
 
 module.exports = {
   loginModule,
   adminloginModule,
   emailModule,
-  Record_apiModule,
+  record_apiModule,
   // 다른 모듈들도 필요한 경우 추가
 };
