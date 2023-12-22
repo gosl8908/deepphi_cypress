548c500133708cc69ee71ff3b9649f0ea9754ca3
gosl8908 <gosl8908@deepnoid.com>
2023-12-22 09:18:53 +0900

    main 커밋

--- cypress/e2e/module/moduleManager.js
+++ cypress/e2e/module/moduleManager.js
@@ -1,10 +1,10 @@
 const loginModule = require("./login.js");
-const adminloginModule = require("./adminlogin.js");
+const adminloginModule = require("./admin-login.js");
 const emailModule = require("./email.js");
-const record_apiModule = require("./record_api.js");
-const image_apiModule = require("./image_api.js");
-const imagedatasetModule = require("./imagedataset.js");
-const recorddatasetModule = require("./recorddataset.js");
+const record_apiModule = require("./record-api.js");
+const image_apiModule = require("./image-api.js");
+const imagedatasetModule = require("./image-dataset.js");
+const recorddatasetModule = require("./record-dataset.js");
 // 다른 모듈들도 필요한 경우 추가
 
 module.exports = {
