35f252bc57cef84f61e68a12a9f301e719b0e44e
gosl8908 <gosl8908@deepnoid.com>
2023-12-28 16:48:16 +0900

    로그 추가

--- cypress/e2e/module/login.module.js
+++ cypress/e2e/module/login.module.js
@@ -1,15 +1,21 @@
 function login(Prod, KangTestId, KangTestPasswd) {
-  cy.log('로그인 성공');
+  cy.log('로그인');
 
   /* 로그인 */
   cy.visit(Prod, { timeout: 120000 }).then(() => {
     // 페이지 로드가 완료되지 않았을 경우 페이지를 새로고침합니다.
     cy.reload();
+    cy.log('Visited the production page.');
 });
-  cy.contains('로그인').click({ timeout: 60000 })// 로그인 클릭
+  cy.contains('로그인').click({ timeout: 60000 })
+  .then(() => {
+    cy.log('Clicked on "로그인".');
+  });// 로그인 클릭
   cy.get('#username').type(KangTestId); // 이메일 입력
   cy.get('#password').type(KangTestPasswd); // 비밀번호 입력
-  cy.get('#kc-login').click({ timeout: 60000 })// 로그인 선택
+  cy.get('#kc-login').click({ timeout: 60000 }).then(() => {
+    cy.log('Clicked on "#kc-login".');
+  });// 로그인 선택
   cy.wait(3000);
 }
 
