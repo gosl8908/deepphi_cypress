fc9416b72e9df4ae6f5fd0eda0b28aaa5343c518
gosl8908 <gosl8908@deepnoid.com>
2024-01-05 17:05:39 +0900

    경로 변경

--- cypress/e2e/project/signup.cy.js
+++ cypress/e2e/project/signup.cy.js
@@ -11,7 +11,7 @@ describe('SignUp', () => {
     it('Create email', () => {
         cy.visit('https://ruu.kr/'); // 일회용 이메일 진입
         /* 환경 변수에서 날짜 레이블 가져오기 */
-        cy.get('#ids').type('test' + Cypress.env('DateLabel')); // 필요한 곳에서 텍스트 사용
+        cy.get('#id').type('test' + Cypress.env('DateLabel')); // 필요한 곳에서 텍스트 사용
         cy.get('#mailList').click();
         cy.wait(5000); // 5초 대기
         // 조합한 텍스트를 파일에 저장
