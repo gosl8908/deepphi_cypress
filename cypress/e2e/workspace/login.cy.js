559f592b5ff58ab9708922cab89d73a7d9e1c98b
gosl8908 <gosl8908@deepnoid.com>
2024-01-08 09:16:41 +0900

    로그인 수정

--- cypress/e2e/workspace/login.cy.js
+++ cypress/e2e/workspace/login.cy.js
@@ -17,24 +17,24 @@ describe('로그인', () => {
       testFailureReason = err.message || '알 수 없는 이유로 실패함'; // 실패 원인을 저장
     });
   });
-  after(() => {
-    let screenshotFileName = `login/login test ${Cypress.env('DateLabel')}`;
-    if (testFailureReason) {
-        // 테스트 실패 시 스크린샷 찍기
-        cy.screenshot(screenshotFileName)
-        // 테스트 실패 시 이메일 전송
-        const EmailBody = `Cypress 자동화 테스트 스위트가 실패하였습니다\n 테스트 실행 시간 : ${Cypress.env(
-          'DateLabelWeek',
-        )}\n 테스트 범위 : 1. 로그인\n\n테스트 실패 원인: ${testFailureReason}`;
-        sendEmailModule.sendEmail(Cypress.env('Id'), 'Login test ' + Cypress.env('EmailTitle'), EmailBody, screenshotFileName);
-    } else {
-      // 테스트가 성공했을 때 이메일 전송
-      const EmailBody = `Cypress 자동화 테스트 스위트가 성공적으로 완료되었습니다\n 테스트 실행 시간 : ${Cypress.env(
-        'DateLabelWeek',
-      )}\n 테스트 범위 : 1. 로그인`;
-      sendEmailModule.sendEmail(Cypress.env('Id'), 'Login test ' + Cypress.env('EmailTitle'), EmailBody);
-    }
-  });
+  // after(() => {
+  //   let screenshotFileName = `login/login test ${Cypress.env('DateLabel')}`;
+  //   if (testFailureReason) {
+  //       // 테스트 실패 시 스크린샷 찍기
+  //       cy.screenshot(screenshotFileName)
+  //       // 테스트 실패 시 이메일 전송
+  //       const EmailBody = `Cypress 자동화 테스트 스위트가 실패하였습니다\n 테스트 실행 시간 : ${Cypress.env(
+  //         'DateLabelWeek',
+  //       )}\n 테스트 범위 : 1. 로그인\n\n테스트 실패 원인: ${testFailureReason}`;
+  //       sendEmailModule.sendEmail(Cypress.env('Id'), 'Login test ' + Cypress.env('EmailTitle'), EmailBody, screenshotFileName);
+  //   } else {
+  //     // 테스트가 성공했을 때 이메일 전송
+  //     const EmailBody = `Cypress 자동화 테스트 스위트가 성공적으로 완료되었습니다\n 테스트 실행 시간 : ${Cypress.env(
+  //       'DateLabelWeek',
+  //     )}\n 테스트 범위 : 1. 로그인`;
+  //     sendEmailModule.sendEmail(Cypress.env('Id'), 'Login test ' + Cypress.env('EmailTitle'), EmailBody);
+  //   }
+  // });
   });
 
 
