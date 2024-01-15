ac62de36e844453e242d6f44b333b70d4919cbc0
gosl8908 <gosl8908@deepnoid.com>
2024-01-16 08:47:23 +0900

    메인 변경

--- cypress/e2e/module/email.module.js
+++ cypress/e2e/module/email.module.js
@@ -1,6 +1,7 @@
 function sendEmail(testFail, id, emailTitle, testRange, screenshotFileName) {
   const isTestFailed  = Boolean(testFail);
   isTestFailed && cy.screenshot(screenshotFileName);
+
   const EmailBody = `Cypress 자동화 테스트 스위트가 ${isTestFailed ? '실패' : '성공'}하였습니다.
   테스트 실행 시간 : ${Cypress.env('DateLabelWeek')}
   테스트 범위 : ${testRange}
@@ -9,17 +10,17 @@ function sendEmail(testFail, id, emailTitle, testRange, screenshotFileName) {
 
   cy.log('테스트가 성공적으로 완료되었습니다.');
 
-  const v = {
+  const sendemail = {
     recipient: id,
     subject: emailTitle,
     body: EmailBody,
 };
 
-if (screenshotFileName) {
-  v.screenshotFileName = screenshotFileName + '.png';
+if (isTestFailed) {
+  sendemail.screenshotFileName = screenshotFileName + '.png';
 }
 
-  cy.task('sendEmail', v).then(success => {
+  cy.task('sendEmail', sendemail).then(success => {
       if (success) {
           cy.log('이메일 전송 성공.');
       } else {
