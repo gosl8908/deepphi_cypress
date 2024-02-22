d77e6e23fa3e5668e6515fd29cc4e47e28775a3f
gosl8908 <gosl8908@deepnoid.com>
2024-02-22 09:46:46 +0900

    로그인 모듈 변경

--- cypress/e2e/module/login.module.js
+++ cypress/e2e/module/login.module.js
@@ -6,9 +6,9 @@ function login(Site, Id, Password) {
             timeout: 30000,
         }).click({ force: true });
     }
-    cy.get('#username').type(Id); // 이메일 입력
-    cy.get('#password').type(Password); // 비밀번호 입력
-    cy.get('#kc-login').click();
+    cy.get('#username', { timeout: 10 * 1000 }).type(Id); // 이메일 입력
+    cy.get('#password', { timeout: 10 * 1000 }).type(Password); // 비밀번호 입력
+    cy.get('#kc-login', { timeout: 10 * 1000 }).click();
     cy.wait(3 * 1000);
     cy.log('로그인 성공');
 }
