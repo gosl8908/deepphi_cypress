1c499e37a5d4c8f611dc5447c623a8d8d01c4199
gosl8908 <gosl8908@deepnoid.com>
2024-02-22 09:46:09 +0900

    펑션 모듈 변경

--- cypress/e2e/module/function.module.js
+++ cypress/e2e/module/function.module.js
@@ -1,10 +1,10 @@
 function getFileName(FilePath) {
-    const currentFilename = FilePath; // 현재 파일의 전체 경로
-    const filenameParts = currentFilename.split('/'); // 경로를 '/'로 나눔 (윈도우의 경우 '\\'로 변경 필요)
+    const CurrentFilename = FilePath; // 현재 파일의 전체 경로
+    const FilenameParts = CurrentFilename.split('/'); // 경로를 '/'로 나눔 (윈도우의 경우 '\\'로 변경 필요)
 
     // 배열의 마지막 요소는 파일명
-    const currentFile = filenameParts[filenameParts.length - 1];
-    return currentFile;
+    const CurrentFile = FilenameParts[FilenameParts.length - 1];
+    return CurrentFile;
 }
 
 module.exports = {
