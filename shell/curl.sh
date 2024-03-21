0daa6e2b6cea3f307f9019f5e8c06300f795cc86
gosl8908 <gosl8908@deepnoid.com>
2024-03-21 09:23:12 +0900

    curl 수정

--- shell/curl.sh
+++ shell/curl.sh
@@ -1,4 +1,4 @@
-curl -vvv --url 'smtp://smtp.office365.com:587' \
+response=$(curl -vvv --url 'smtp://smtp.office365.com:587' \
     --ssl-reqd \
     --mail-from 'gosl8908@deepnoid.com' \
     --mail-rcpt 'gosl8908@deepnoid.com' \
@@ -6,4 +6,8 @@ curl -vvv --url 'smtp://smtp.office365.com:587' \
     -F "=@./cypress/reports/html/index.html" \
     -H "Subject: [$date] $subject 자동화 테스트 결과" \
     -H "From: <gosl8908@deepnoid.com>" \
-    -H "To:<gosl8908@deepnoid.com>"
+    -H "To:<gosl8908@deepnoid.com>")
+
+if echo "$response" | grep -q "Login denied"; then
+    echo "계정 정보가 잘못되었습니다. 다시 확인해주세요."
+fi
